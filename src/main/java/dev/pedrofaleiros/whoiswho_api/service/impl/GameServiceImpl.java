11e0fd3ecf179c12626264b752c46e8425aee0e4
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2024-10-12 11:35:04 -0300

    impostors

--- src/main/java/dev/pedrofaleiros/whoiswho_api/service/impl/GameServiceImpl.java
+++ src/main/java/dev/pedrofaleiros/whoiswho_api/service/impl/GameServiceImpl.java
@@ -192,7 +192,7 @@ public class GameServiceImpl implements GameService {
     }
 
     private void validateImpostors(Room room, int usersSize) {
-        if (usersSize < 3 || room.getImpostors() > usersSize / 2) {
+        if (usersSize < 3 || room.getImpostors() >= usersSize / 2) {
             throw new WsWarningException("Impostores devem ser minoria");
         }
 
